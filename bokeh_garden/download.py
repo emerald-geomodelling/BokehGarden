6afc204b06e3b625cdd63007e50c31f889b3f54b
Egil <egil.moller@freecode.no>
2020-10-01 14:03:30 +0200

    Bugfixes

--- bokeh_garden/download.py
+++ bokeh_garden/download.py
@@ -7,6 +7,7 @@ import tornado.ioloop
 import bokeh.core.properties
 import weakref
 import uuid
+import sys
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -16,19 +17,29 @@ class MainHandler(tornado.web.RequestHandler):
         else:
             self.set_status(404, "Unknown download ID %s (existing: %s)" % (download_id, ",".join(str(key) for key in downloads.keys())))
 
-loop = tornado.ioloop.IOLoop.current()
-for fd, (sock, handler) in list(loop.handlers.items()):
-    try:
-        if isinstance(sock, socket.socket) and sock.getsockname()[1] == 8912:
-            loop.close_fd(fd)
-            loop.remove_handler(fd)
-    except:
-        loop.remove_handler(fd)
-            
-tornado.web.Application([
+app = tornado.web.Application([
     (r"/.*", MainHandler),
-]).listen(8912)
+])
 
+if hasattr(sys, "_bokeh_garden_download_port"):
+    loop = tornado.ioloop.IOLoop.current()
+    for fd, (sock, handler) in list(loop.handlers.items()):
+        try:
+            if isinstance(sock, socket.socket) and sock.getsockname()[1] == sys._bokeh_garden_download_port:
+                loop.close_fd(fd)
+                loop.remove_handler(fd)
+        except:
+            loop.remove_handler(fd)
+
+sys._bokeh_garden_download_port = 8912
+for attempt in range(100):
+    try:
+        app.listen(sys._bokeh_garden_download_port)
+    except:
+        sys._bokeh_garden_download_port += 1
+    else:
+        break
+    
 downloads = weakref.WeakValueDictionary()
 
 class Download(bokeh.models.Div):
@@ -37,7 +48,7 @@ class Download(bokeh.models.Div):
     __subtype__ = "Download"
 
     content = bokeh.core.properties.Any(serialized=False)
-    name = bokeh.core.properties.String(default="file.txt", serialized=False)
+    filename = bokeh.core.properties.String(default="file.txt", serialized=False)
 
     def __init__(self, **kw):
         self._download_id = str(uuid.uuid4())
@@ -47,5 +58,5 @@ class Download(bokeh.models.Div):
         self.text = "<a href='http://localhost:8912/%s' target='_new'>%s</a>" % (self._download_id, self.text)
 
     def get(self, request_handler):
-        request_handler.add_header("Content-Disposition", 'attachment; filename="%s"' % self.name)
+        request_handler.add_header("Content-Disposition", 'attachment; filename="%s"' % self.filename)
         request_handler.write(bytes(self.content))
