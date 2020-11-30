061cc6479e384c3e2898da88fe077915ed8469a1
RedHog <em@emrld.no>
2020-11-30 14:43:38 +0000

    Bugfixes

--- bokeh_garden/serverutils.py
+++ bokeh_garden/serverutils.py
@@ -5,10 +5,13 @@ def current_bokeh_tornado_server(doc = None):
     the one associated with a particular bokeh Document"""
     if doc is None:
         doc = bokeh.plotting.curdoc()
-    doc_port = int(doc.session_context.request.headers["Host"].split(":")[-1])
+    host = doc.session_context.request.headers["Host"]
+    doc_port = None
+    if ":" in host:
+        doc_port = int(host.split(":")[-1])
     for s, handler in doc.session_context.server_context.application_context.io_loop.handlers.values():
         if not hasattr(s, "getsockname"): continue
-        if s.getsockname()[1] != doc_port: continue
+        if doc_port is not None and s.getsockname()[1] != doc_port: continue
         connection_handler = handler.__closure__[0].cell_contents # tornado.httpserver.HTTPServer._handle_connection
         http_server = connection_handler.__self__                 # tornado.httpserver.HTTPServer
         bokeh_tornado = http_server.request_callback              # bokeh.server.tornado.BokehTornado
