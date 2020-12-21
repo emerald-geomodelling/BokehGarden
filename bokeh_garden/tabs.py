502bda5c137f8060f47f78797d206813db1828a5
Egil <egil.moller@freecode.no>
2020-12-21 10:17:53 +0100

    Bugfix for tabs attributes

--- bokeh_garden/tabs.py
+++ bokeh_garden/tabs.py
@@ -6,6 +6,7 @@ class Tabs(bokeh.models.widgets.Tabs):
     __view_module__ = bokeh.models.widgets.Tabs.__view_module__
     __subtype__ = 'BokehGardenTabs'
     def __init__(self, **kw):
-        bokeh.models.widgets.Tabs.__init__(self)
+        bokeh.models.widgets.Tabs.__init__(self, **{name: item for name, item in kw.items()
+                                                    if not isinstance(item, bokeh.models.layouts.LayoutDOM)})
         self.tabs = [bokeh.models.widgets.Panel(child=item, title=name)
                      for name, item in kw.items() if isinstance(item, bokeh.models.layouts.LayoutDOM)]
