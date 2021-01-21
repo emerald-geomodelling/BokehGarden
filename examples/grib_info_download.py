cf09ebd317f60fd0cd5c763267380b41764fe730
glinares-em <gl@emeraldgeo.com>
2021-01-21 09:49:09 +0100

    first commit to logging branch

--- examples/grib_info_download.py
+++ examples/grib_info_download.py
@@ -5,11 +5,30 @@ import tempfile
 import pygrib
 import bokeh.models.sources
 import io
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
+
+# logger = logging.getLogger(__name__)
+#logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+# #
+file_handler = logging.FileHandler('test2.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.ERROR)
+
+logger.addHandler(file_handler)
+
 
 def get_grib_layer_info(grib):
+    print("get grib layer info")
     layerdatas = []
     for layeridx in range(1, grib.messages + 1):
         layer = grib.message(layeridx)
+        logger.info('Get grib layer info')
         def getval(g, name):
             try:
                 return g[name]
@@ -18,20 +37,47 @@ def get_grib_layer_info(grib):
         layerdatas.append({key:value for key, value in ((key, getval(layer, key)) for key in layer.keys())
                            if not hasattr(value, "__len__") or len(value) < 20})
     return pd.DataFrame(layerdatas)
+    logger.info('DataFrame')
 
 class DownloadContent(object):
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.INFO)
+    # formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
     def __init__(self, app):
         self.app = app
-        
+        logger.info('Download Content')
+        try:
+            print(y)
+        except NameError:
+            logger.exception('Another exception occurred')
+
     def __bytes__(self):
+        print("bytes")
         layer_info = get_grib_layer_info(self.app.grib.grib)
+        logger.info('Bytes')
+        print("layer info", layer_info)
         with io.StringIO() as f:
             layer_info.to_csv(f)
-            return f.getvalue().encode("utf-8")   
+            F = f.getvalue().encode("utf-8")
+
+            print(F)
+            return F
+
+
 
 class GribInfoDownload(bokeh_garden.application.AppWidget, bokeh.models.layouts.Column):
+    # logger = logging.getLogger(__name__)
+    # logger.setLevel(logging.INFO)
+    # formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
     def __init__(self, app, **kw):
         self._app = app
+        logger.info('GRIB info download')
+        try:
+            print(y)
+        except NameError:
+            logger.exception('Another exception occurred')
 
         self._link = bokeh_garden.download.Download(
             text="Download layer information as CSV",
@@ -39,3 +85,4 @@ class GribInfoDownload(bokeh_garden.application.AppWidget, bokeh.models.layouts.
             content=DownloadContent(self._app))
 
         bokeh.models.layouts.Column.__init__(self, self._link, **kw)
+
--- examples/weather_data_explorer.py
+++ examples/weather_data_explorer.py
@@ -3,16 +3,32 @@ import bokeh_garden
 import examples.grib_plot
 import examples.grib_upload
 import examples.grib_info_download
+import logging
+import examples.grib_log
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
+#file_handler = logging.FileHandler('test2.log')
+#file_handler.setFormatter(formatter)
+#
+# file_handler = logging.FileHandler('sample.log')
+# file_handler.setLevel(logging.ERROR)
+# file_handler.setFormatter(formatter)
+#logger.addHandler(file_handler)
 
 default_layout = {"widget": bokeh_garden.tabs.Tabs,
                   "Chose file": {"widget": bokeh.models.layouts.Column,
                                  "children": [{"widget": examples.grib_upload.GribUpload},
                                               {"widget": examples.grib_info_download.GribInfoDownload}]},
-                  "Plot": {"widget": examples.grib_plot.GribPlot}}
+                  "Plot": {"widget": examples.grib_plot.GribPlot},
+                  "Log": {"widget": examples.grib_log.GribLog}}
 
 class WeatherDataExplorer(bokeh_garden.application.Application):
     def __init__(self, layout = default_layout, **kw):
         bokeh_garden.application.Application.__init__(self, layout=layout, **kw)
+        logger.info('Weather Data Explorer')
 
 if __name__.startswith("bokeh_app_"):
     # We got started using `bokeh serve examples/weather_data_explorer.py`
